5a50f2a0d06eaeffbfe47ed60cab0b4a8583427f
Victor Marques <victor.marques@docway.co>
2020-06-04 20:37:07 -0300

    desafio concluído

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import api from './services/api'
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories')
+      .then(response => {
+        setRepositories(response.data)
+      })
+  }, [])
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: `Novo projeto ${Date.now()}`,
+      url: "https://github.com/victormarques92/rocket",
+      techs: ["ReactJS", "Styled Components"],
+      likes: 0
+    })
+
+    const repository = response.data
+
+    setRepositories([ ...repositories, repository ])
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(
+      repo => repo.id !== id
+    ))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repo => (
+            <li key={repo.id}>
+              {repo.title}
+
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
